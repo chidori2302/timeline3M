913edd6c9a8deeeb10cdf57c3aba9b49d1a910e8
Chidori <chidung200150@gmail.com>
2022-08-27 20:26:35 +0700

    ok

--- scriptTL.js
+++ scriptTL.js
@@ -1,34 +1,50 @@
-(function () {
-    "use strict";
-  
-    // define variables
-    var items = document.querySelectorAll(".timeline-item");
-  
-    function isElementInViewport(el) {
-      var rect = el.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-          (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
+function isElementInViewport(el) {
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+
+  while(el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return (
+    top >= window.pageYOffset &&
+    left >= window.pageXOffset &&
+    (top + height) <= (window.pageYOffset + window.innerHeight) &&
+    (left + width) <= (window.pageXOffset + window.innerWidth)
+  );
+}
+
+
+var items = document.querySelectorAll(".timeline-item");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+
+function callbackFunc(){
+  items.forEach((item, index)=>{
+    if (isElementInViewport(item)) {
+      if (index % 2 == 0){item.classList.add("activeR");}
+      else {item.classList.add("activeL");};
     }
+  })
+}
+
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
   
-    function callbackFunc() {
-      for (var i = 0; i < items.length; i++) {
-        if (isElementInViewport(items[i])) {
 
-            console.log(i);
 
-          items[i].classList.add("timeline-item");
-        }
-      }
-    }
-  
-    // listen for events
 
-    window.addEventListener("load", callbackFunc);
-    window.addEventListener("resize", callbackFunc);
-    window.addEventListener("scroll", callbackFunc);
-  })();
+
+
+
+
+
+
